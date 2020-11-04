6c6df3d50a9cd83e6f5dba31d7933a2de258e0b9
Callum Powley <callum.powley@gmail.com>
2020-11-04 16:56:06 +0000

    Added q learning and it works

--- Python Code/LearningBot.py
+++ Python Code/LearningBot.py
@@ -1,11 +1,45 @@
 import random
 import numpy as np
-#import pandas as pd
+import pandas as pd
 import os
 from absl import app
 from pysc2.agents import base_agent
 from pysc2.lib import actions, features, units
 from pysc2.env import sc2_env, run_loop
+
+
+class QLearningTable:
+  def __init__(self, actions, learning_rate=0.01, reward_decay=0.9):
+    self.actions = actions
+    self.learning_rate = learning_rate
+    self.reward_decay = reward_decay
+    self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+
+  def choose_action(self, observation, e_greedy=0.9):
+    self.check_state_exist(observation)
+    if np.random.uniform() < e_greedy:
+      state_action = self.q_table.loc[observation, :]
+      action = np.random.choice(
+          state_action[state_action == np.max(state_action)].index)
+    else:
+      action = np.random.choice(self.actions)
+    return action
+
+  def learn(self, s, a, r, s_):
+    self.check_state_exist(s_)
+    q_predict = self.q_table.loc[s, a]
+    if s_ != 'terminal':
+      q_target = r + self.reward_decay * self.q_table.loc[s_, :].max()
+    else:
+      q_target = r
+    self.q_table.loc[s, a] += self.learning_rate * (q_target - q_predict)
+
+  def check_state_exist(self, state):
+    if state not in self.q_table.index:
+      self.q_table = self.q_table.append(pd.Series([0] * len(self.actions), 
+                                                   index=self.q_table.columns, 
+                                                   name=state))
+
 
 class Agent(base_agent.BaseAgent):
   actions = ("do_nothing",
@@ -128,9 +162,6 @@ class Agent(base_agent.BaseAgent):
     return actions.RAW_FUNCTIONS.no_op()
 
 
-    
-
-
 class RandomAgent(Agent):
   def step(self, obs):
     super(RandomAgent, self).step(obs)
@@ -138,8 +169,93 @@ class RandomAgent(Agent):
     return getattr(self, action)(obs)
 
 
+class SmartAgent(Agent):
+  def __init__(self):
+    super(SmartAgent, self).__init__()
+    self.qtable = QLearningTable(self.actions)
+    self.new_game()
+
+  def reset(self):
+    super(SmartAgent, self).reset()
+    self.new_game()
+    
+  def new_game(self):
+    self.base_top_left = None
+    self.previous_state = None
+    self.previous_action = None
+
+  def get_state(self, obs):
+    scvs = self.get_my_units_by_type(obs, units.Terran.SCV)
+    idle_scvs = [scv for scv in scvs if scv.order_length == 0]
+    command_centers = self.get_my_units_by_type(obs, units.Terran.CommandCenter)
+    supply_depots = self.get_my_units_by_type(obs, units.Terran.SupplyDepot)
+    completed_supply_depots = self.get_my_completed_units_by_type(
+        obs, units.Terran.SupplyDepot)
+    barrackses = self.get_my_units_by_type(obs, units.Terran.Barracks)
+    completed_barrackses = self.get_my_completed_units_by_type(
+        obs, units.Terran.Barracks)
+    marines = self.get_my_units_by_type(obs, units.Terran.Marine)
+    
+    queued_marines = (completed_barrackses[0].order_length 
+                      if len(completed_barrackses) > 0 else 0)
+    
+    free_supply = (obs.observation.player.food_cap - 
+                   obs.observation.player.food_used)
+    can_afford_supply_depot = obs.observation.player.minerals >= 100
+    can_afford_barracks = obs.observation.player.minerals >= 150
+    can_afford_marine = obs.observation.player.minerals >= 100
+    
+    enemy_scvs = self.get_enemy_units_by_type(obs, units.Terran.SCV)
+    enemy_idle_scvs = [scv for scv in enemy_scvs if scv.order_length == 0]
+    enemy_command_centers = self.get_enemy_units_by_type(
+        obs, units.Terran.CommandCenter)
+    enemy_supply_depots = self.get_enemy_units_by_type(
+        obs, units.Terran.SupplyDepot)
+    enemy_completed_supply_depots = self.get_enemy_completed_units_by_type(
+        obs, units.Terran.SupplyDepot)
+    enemy_barrackses = self.get_enemy_units_by_type(obs, units.Terran.Barracks)
+    enemy_completed_barrackses = self.get_enemy_completed_units_by_type(
+        obs, units.Terran.Barracks)
+    enemy_marines = self.get_enemy_units_by_type(obs, units.Terran.Marine)
+    
+    return (len(command_centers),
+            len(scvs),
+            len(idle_scvs),
+            len(supply_depots),
+            len(completed_supply_depots),
+            len(barrackses),
+            len(completed_barrackses),
+            len(marines),
+            queued_marines,
+            free_supply,
+            can_afford_supply_depot,
+            can_afford_barracks,
+            can_afford_marine,
+            len(enemy_command_centers),
+            len(enemy_scvs),
+            len(enemy_idle_scvs),
+            len(enemy_supply_depots),
+            len(enemy_completed_supply_depots),
+            len(enemy_barrackses),
+            len(enemy_completed_barrackses),
+            len(enemy_marines))
+
+  def step(self, obs):
+    super(SmartAgent, self).step(obs)
+    state = str(self.get_state(obs))
+    action = self.qtable.choose_action(state)
+    if self.previous_action is not None:
+      self.qtable.learn(self.previous_state,
+                        self.previous_action,
+                        obs.reward,
+                        'terminal' if obs.last() else state)
+    self.previous_state = state
+    self.previous_action = action
+    return getattr(self, action)(obs)
+
+
 def main(unused_argv):
-  agent1 = RandomAgent()
+  agent1 = SmartAgent()
   agent2 = RandomAgent()
   try:
     with sc2_env.SC2Env(
