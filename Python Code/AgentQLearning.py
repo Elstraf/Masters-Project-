9492cc13ea231757eee0d2093052f17a615a8742
Callum Powley <callum.powley@gmail.com>
2020-12-01 15:00:24 +0000

    Finished Attack Funtion (Basic)

--- Python Code/AgentQLearning.py
+++ Python Code/AgentQLearning.py
@@ -1,10 +1,17 @@
 #Imports 
-import numpy 
+import numpy as np 
 
 from pysc2.agents import base_agent
 from pysc2.lib import actions, features, units
 
 from QLearning import QLearningTable 
+
+
+#Gets all enemies on the screen 
+_PLAYER_ENEMY = features.PlayerRelative.ENEMY
+
+#Gets all player units on screen 
+_PLAYER_SELF = features.PlayerRelative.SELF
 
 
 class QLearningAgent(base_agent.BaseAgent):
@@ -20,12 +27,25 @@ class QLearningAgent(base_agent.BaseAgent):
                 if unit.unit_type == unit_type
                 and unit.alliance == features.PlayerRelative.SELF]
 
+    def get_distances(self, obs, units, xy):
+        units_xy = [(unit.x, unit.y) for unit in units]
+        return np.linalg.norm(np.array(units_xy) - np.array(xy), axis=1)
+
     def do_nothing(self, obs):
         return actions.RAW_FUNCTIONS.no_op()
 
     def attack(self, obs):
         marines = self.get_my_units_by_type(obs, units.Terran.Marine)
+        if len(marines) > 0:
+            roaches = [units for unit in obs.observation.raw_units
+                       if unit.alliance == _PLAYER_ENEMY]
+            distances = self.get_distances(obs, marines, roaches)
+            marine = marines[np.argmax(distances)]
 
+            return actions.RAW_FUNCTIONS.Attack_pt(
+                "now", marine.tag, (roaches[0])
+            )
+        return actions.RAW_FUNCTIONS.no_op()
 
 
     def step(self, obs):
