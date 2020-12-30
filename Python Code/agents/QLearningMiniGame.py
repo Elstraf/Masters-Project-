d8aba35cd24289328d39bc1e1b252b400ff6f738
Callum Powley <callum.powley@gmail.com>
2020-12-30 13:17:32 +0000

    simple retreat that goes to a spot on the map

--- Python Code/agents/QLearningMiniGame.py
+++ Python Code/agents/QLearningMiniGame.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import pandas as pd
+import random 
 
 from pysc2.agents import base_agent
 from pysc2.lib import actions 
@@ -78,12 +79,10 @@ class Agent(base_agent.BaseAgent):
 
         self.qlearn = QLearningTable(self.smartActions)
 
-
-    def step(self, obs):
-        super(Agent, self).step(obs)
+    def attack(self, obs):
 
         if actions.FUNCTIONS.Attack_screen.id in obs.observation.available_actions:
-            player_relative = obs.observation.feature_screen.player_relative 
+            player_relative = obs.observation.feature_screen.player_relative
             targets = _xy_locs(player_relative == features.PlayerRelative.ENEMY)
 
             if not targets:
@@ -94,8 +93,30 @@ class Agent(base_agent.BaseAgent):
 
         if actions.FUNCTIONS.select_army.id in obs.observation.available_actions:
             return actions.FUNCTIONS.select_army("select")
-
+        
         return actions.FUNCTIONS.no_op()
+
+    def retreat(self, obs):
+
+        if actions.FUNCTIONS.Move_screen.id in obs.observation.available_actions:
+
+            player_relative = obs.observation.feature_screen.player_relative
+            enemies = _xy_locs(player_relative == features.PlayerRelative.ENEMY)
+
+            if not enemies:
+                return actions.FUNCTIONS.no_op()
+            
+            moveAway = (random.randint(0, 100), random.randint(0,100))         
+
+            return actions.FUNCTIONS.Move_screen("now", moveAway)
+
+
+        return actions.FUNCTIONS.select_army("select")
+
+
+
+    def step(self, obs):
+        super(Agent, self).step(obs)
 
         # Gets all the players units on screen
         # Since its just one set of units we dont need to check for certain types 
@@ -108,16 +129,9 @@ class Agent(base_agent.BaseAgent):
      #   targets = [unit for unit in obs.observation.raw_units
               #  if unit.alliance == features.PlayerRelative.ENEMY]
         
-
-        #Checks to see if theyre not null
-       # if marines and targets:
-            #target = targets[0]
-            #distances = self.get_distances(obs, marines, targets[0])
-            #targetLocation = targets[np.argmax(distances)]
-            #targetXY = [targets[0].x, targets[0].y]
-           # return actions.RAW_FUNCTIONS.Attack_pt("now", marines,(targetXY))
-
         #return actions.RAW_FUNCTIONS.no_op()
+
+                # select the user units 
 
 
         
